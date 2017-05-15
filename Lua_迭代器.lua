7871aea5a93dab3cb2ce2c003fc62cca3218963e
Administrator <chinalzge@gmail.com>
2017-05-15 22:34:51 +0800

    add 迭代器

---
+++ Lua_迭代器.lua
@@ -0,0 +1,69 @@
+
+
+print("--泛型 for 迭代器--")
+
+array = {"Lua", "Tutorial"}
+
+for key,value in ipairs(array)
+do
+   print(key, value)
+end
+
+
+
+print("--无状态的迭代器--")
+
+print("--数字 n 的平方--")
+function square(iteratorMaxCount,currentNumber)
+   if currentNumber<iteratorMaxCount
+   then
+      currentNumber = currentNumber+1
+   return currentNumber, currentNumber*currentNumber
+   end
+end
+
+for i,n in square,3,0
+do
+   print(i,n)
+end
+print("---------")
+
+function iter (a, i)
+    i = i + 1
+    local v = a[i]
+    if v then
+       return i, v
+    end
+end
+
+function ipairs (a)
+    return iter, a, 0
+end
+
+
+
+
+print("--多状态的迭代器--")
+
+
+array = {"Lua", "Tutorial"}
+
+function elementIterator (collection)
+   local index = 0
+   local count = #collection
+   -- 闭包函数
+   return function ()
+      index = index + 1
+      if index <= count
+      then
+         --  返回迭代器的当前元素
+         return collection[index]
+      end
+   end
+end
+
+for element in elementIterator(array)
+do
+   print(element)
+end
+
